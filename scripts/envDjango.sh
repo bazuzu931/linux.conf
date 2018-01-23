dd49cd085285bc7f69a049f0957e2b73e2f90ab7
bazuzu931 <bazuzu931@yandex.ru>
2018-01-23 11:41:35 +0500

    push new conf

--- scripts/envDjango.sh
+++ scripts/envDjango.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Usage
+#
+# For example create function-alias in .bashrc : 
+# function envdj { source /home/$USER/Desktop/github_projects/linux.conf/scripts/envDjango.sh }
+#
+# Run script
+# $ envdj env_name project_name django_version
+
+
+
 # create and activate python environment
 envpy $1;
 
@@ -11,18 +21,24 @@ sudo apt-get -y install fabric
 django-admin startproject $2; cd $2;
 pip freeze > requirements.txt;
 
+# add project folders for initial structuring
 mkdir media static static/css static/js static/image;
-touch static/css/style.css  static/js/main.js .gitignore fabfile.py example_fabfile.py $2/local_settings.py;
-printf "*.pyc\nfabfile.py\n$2/media\n$2/local_settings.py" >> .gitignore;
-printf "\nSTATIC_ROOT = os.path.join(BASE_DIR, '$2', 'static')\n\nMEDIA_URL = '/media/'\n\nMEDIA_ROOT = os.path.join(BASE_DIR, '$2', 'media')" >> $2/settings.py
-	
 
+# add local_settings
+touch static/css/style.css  static/js/main.js .gitignore fabfile.py example_fabfile.py $2/local_settings.py;
+
+# add .gitignore
+printf "*.pyc\nfabfile.py\n$2/media\n$2/local_settings.py" >> .gitignore;
+	
+# add to django setting
+printf "\nSTATIC_ROOT = os.path.join(BASE_DIR, '$2', 'static')\n\nMEDIA_URL = '/media/'\n\nMEDIA_ROOT = os.path.join(BASE_DIR, '$2', 'media')" >> $2/settings.py
 printf "\n\nSTATICFILES_DIRS = [
 os.path.join(BASE_DIR, 'static'),
 '/$2/static/',
 ]" >> $2/settings.py
 	
 
+# create fabfile.py
 printf "
 import os
 from fabric.api import run, env, cd, roles, lcd, local, sudo
--- scripts/envNode.sh
+++ scripts/envNode.sh
@@ -3,12 +3,15 @@
 # create project folder
 mkdir $1; cd $1;
 
-# 
+# install new node version
 nvm install $2;
 
-# touch .nvmrc;
+# touch .nvmrc for adding new installed node version
 echo $2 > .nvmrc;
+
+# Use installed node version
 nvm use;
 
-printf 'console.log(`Current nodejs version: ${process.versions.node}`);'  > check_version.js;
-node check_version.js;
+# add a js file for checking current verison of node.js
+printf 'console.log(`Current nodejs version: ${process.versions.node}`);'  > node_check_version.js;
+node node_check_version.js;
