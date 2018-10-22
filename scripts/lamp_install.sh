17cce1c4a4f97094bb5ec63dc8fc51fa520fd1d6
bazuzu931 <bazuzu931@yandex.ru>
2018-10-23 00:48:41 +0400

    push new conf

--- scripts/lamp_install.sh
+++ scripts/lamp_install.sh
@@ -25,7 +25,15 @@ sudo apt-get upgrade -y;
 sudo apt-get install apache2 -y;
 sudo chmod -R /var/www/html ;
 sudo apt-get install mysql-server mysql-client -y;
-sudo apt-get install php7.0 php7.0-mysql libapache2-mod-php7.0 -y;
+# sudo apt-get install php7.0 php7.0-mysql libapache2-mod-php7.0 -y;
+
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update
+sudo apt install php7.1
+sudo apt install php7.1-cli php7.1-common php7.1-json php7.1-opcache php7.1-mysql php7.1-mbstring php7.1-mcrypt php7.1-zip
+sudo a2enmod php7.1
+sudo systemctl restart apache2.service
+
 sudo apt-get install php-zip
 sudo apt-get install phpmyadmin -y;
 sudo apt-get install phpunit -y;
@@ -54,3 +62,14 @@ sudo systemctl reload apache2;
 
 sudo apt-get install -y php-xdebug;
 sudo /etc/init.d/apache2 restart;
+
+
+################################################################################
+#### Reset mysql root
+# use mysql;
+# UPDATE user SET plugin='mysql_native_password' WHERE User='root';
+# FLUSH PRIVILEGES;
+# exit;
+#### Then enable NOPASS FOR ROOT in /etc/phpmyadmin/config.inc.php
+#### Then go run phpmyadmin in browser and sign in as root without pass!
+################################################################################
